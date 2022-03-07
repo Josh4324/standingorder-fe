de0f8d2142f00a525257df8f9228c32c40f5cc74
Joshua Adesanya <adesanyajoshua@ymail.com>
2022-03-07 13:54:32 +0100

    chnages

--- src/containers/TheLayout.js
+++ src/containers/TheLayout.js
@@ -1,60 +1,48 @@
-import React from 'react'
-import {
-  TheContent,
-  TheSidebar,
-  TheFooter,
-  TheHeader
-} from './index';
+import React from "react";
+import { TheContent, TheSidebar, TheFooter, TheHeader } from "./index";
 import { useHistory } from "react-router-dom";
-import { NotificationManager} from 'react-notifications';
-import { useIdleTimer } from 'react-idle-timer';
-
-  
+import { NotificationManager } from "react-notifications";
+import { useIdleTimer } from "react-idle-timer";
 
 const TheLayout = () => {
   let history = useHistory();
 
-  if (localStorage.getItem("stand-order-token") === null){
-      history.push("/login")
+  if (localStorage.getItem("stand-order-token") === null) {
+    history.push("/login");
   }
 
-  const handleOnIdle = event => {
-      localStorage.removeItem("stand-order-token")
-      history.push("/login");
-      NotificationManager.success('User is Idle', 'TimeOut');
-   
-  }
+  const handleOnIdle = (event) => {
+    localStorage.removeItem("stand-order-token");
+    history.push("/login");
+    NotificationManager.success("User is Idle", "TimeOut");
+  };
 
-  const handleOnActive = event => {
-   
-   
-  }
+  const handleOnActive = (event) => {};
 
   const handleOnAction = (e) => {
     //console.log('user did something', e)
-    
-  }
+  };
 
   const { getRemainingTime, getLastActiveTime } = useIdleTimer({
     timeout: 1000 * 60 * 5,
     onIdle: handleOnIdle,
     onActive: handleOnActive,
     onAction: handleOnAction,
-    debounce: 500
-  })
+    debounce: 500,
+  });
 
   return (
     <div className="c-app c-default-layout">
-      <TheSidebar/>
+      <TheSidebar />
       <div className="c-wrapper">
-        <TheHeader/>
+        <TheHeader />
         <div className="c-body">
-          <TheContent/>
+          <TheContent />
         </div>
-        <TheFooter/>
+        <TheFooter />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default TheLayout
+export default TheLayout;
